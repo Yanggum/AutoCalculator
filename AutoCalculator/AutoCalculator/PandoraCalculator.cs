07dc27895bd33be12f1d29c1f95521f30fc7b3d2
박창원 <bak405@naver.com>
2020-05-24 10:09:41 +0900

    테슬라 코드 완성

---
+++ AutoCalculator/AutoCalculator/PandoraCalculator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace AutoCalculator
+{
+    public class PandoraCalculator
+    {
+        public static double GetTeslaCode(string param)
+        {
+            var result = 0.0;
+            var length = 0;
+
+            if (param.Contains('.'))
+            {
+                length = param.Split('.')[1].Length;
+                result = Double.Parse(param);
+                for (var i=0; i<length; i++)
+                {
+                    result = result * 10;
+                }
+            }
+            else
+            {
+                result = Int32.Parse(param) > 9 ? Int32.Parse(param) : Int32.Parse(param) * 2;
+            }
+
+            result = Math.Round(result);
+
+            if (result >= 10)
+            {
+                int cnt = result.ToString().Length;
+
+                while (result >= 10)
+                {
+                    var arr = new ArrayList();
+                    var temp = result;
+
+                    // 한 자리 수로 만드는 알고리즘
+                    for (var i = cnt; i > 0; i--)
+                    {
+                        arr.Add((int)(temp / (int)Math.Pow(10, i - 1)));
+                        temp = temp % (int)Math.Pow(10, i - 1);
+                    }
+
+                    int sum = 0;
+                    for (var i = 0; i < arr.Count; i++)
+                    {
+                        sum += (int)arr.ToArray()[i];
+                    }
+
+                    result = sum;
+                }
+            }
+
+            return result;
+        }
+
+
+    }
+}
--- AutoCalculator/AutoCalculator/Program.cs
+++ AutoCalculator/AutoCalculator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Runtime.InteropServices.ComTypes;
 
 namespace AutoCalculator
 {
@@ -15,73 +16,10 @@ namespace AutoCalculator
 
             while (true)
             {
-                var rand = new Random();
-                oper = rand.Next(0, 3);
-                test = rand.Next(1, 9);
-                test2 = rand.Next(1, 9);
-                //                result = 
-
-                switch (oper)
-                {
-                    // +
-                    case 0:
-                        result = test + test2;
-                        operString = "+";
-                        break;
-                    // -
-                    case 1:
-                        result = test - test2;
-                        operString = "-";
-                        break;
-                    // *
-                    case 2:
-                        result = test * test2;
-                        operString = "*";
-                        break;
-                    // /
-                    case 3:
-                        result = test / test2;
-                        operString = "/";
-                        break;
-                }
-
-                Console.WriteLine(test + " " + operString + " " + test2 + " = " + result);
-
+                Console.Write("숫자를 입력하세요: ");
+                operString = Console.ReadLine();
+                Console.WriteLine(PandoraCalculator.GetTeslaCode(operString));
             }
         }
-
-        static int GetTeslaCode(string param)
-        {
-            var result = Int32.Parse(param) > 9 ? Int32.Parse(param) : Int32.Parse(param) * 2;
-
-            if (result >= 10)
-            {
-                int cnt = result.ToString().Length;
-
-                while (result >= 10)
-                {
-                    var arr = new ArrayList();
-                    var temp = result;
-
-                    // 한 자리 수로 만드는 알고리즘
-                    for (var i = cnt; i > 0; i--)
-                    {
-                        arr.Add(temp / (int)Math.Pow(10, i - 1));
-                        temp = temp % (int)Math.Pow(10, i - 1);
-                    }
-
-                    int sum = 0;
-                    for (var i = 0; i < arr.Count; i++)
-                    {
-                        sum += (int)arr.ToArray()[i];
-                    }
-
-                    result = sum;
-                }
-            }
-
-            return result;
-        }
-
     }
 }
