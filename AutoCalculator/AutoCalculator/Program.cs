18201205a4b45bd46dd25af759fdb689e96dcd1d
박창원 <bak405@naver.com>
2020-08-18 11:12:19 +0900

    From Singularity

--- AutoCalculator/AutoCalculator/Program.cs
+++ AutoCalculator/AutoCalculator/Program.cs
@@ -1,25 +1,64 @@
 ﻿using System;
 using System.Collections;
 using System.Runtime.InteropServices.ComTypes;
+using System.Threading;
 
 namespace AutoCalculator
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void SingularityProc()
         {
-            var test = 0;
-            var test2 = 0;
-            var result = 0;
-            var oper = -1;
-            var operString = "";
+            var charList = new ArrayList();
+
+            var rand = new Random();
+            bool isSeedTriggered = true;
+            double seed = 0.0f;
 
             while (true)
             {
-                Console.Write("숫자를 입력하세요: ");
-                operString = Console.ReadLine();
-                Console.WriteLine(PandoraCalculator.GetTeslaCode(operString));
+                if (isSeedTriggered)
+                {
+                    seed = 1;
+                    isSeedTriggered = false;
+                }
+
+                seed = PandoraCalculator.GetTeslaCode(seed);
+                Console.WriteLine(seed);
             }
+        }
+
+        public static void SingularityProc3()
+        {
+            var charList = new ArrayList();
+
+            var rand = new Random();
+            bool isSeedTriggered = true;
+            double seed = 0.0f;
+
+            while (true)
+            {
+                if (isSeedTriggered)
+                {
+                    seed = 3;
+                    isSeedTriggered = false;
+                }
+
+                seed = PandoraCalculator.GetTeslaCode(seed);
+                Console.WriteLine(seed);
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(new ThreadStart(SingularityProc));
+            Thread t2 = new Thread(new ThreadStart(SingularityProc3));
+
+            t1.Start();
+            t2.Start();
+
+
         }
     }
 }
