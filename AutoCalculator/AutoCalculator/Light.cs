ffbddb89c58220f8d2bb36941d67031047c96a4d
박창원 <bak405@naver.com>
2020-11-02 22:09:45 +0900

    Dark Light Origin 추가

---
+++ AutoCalculator/AutoCalculator/Light.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoCalculator
+{
+    public class Light : Origin
+    {
+        public double Create()
+        {
+            var rand = new Random();
+
+            return value + rand.NextDouble();
+        }
+    }
+}
--- AutoCalculator/AutoCalculator/Program.cs
+++ AutoCalculator/AutoCalculator/Program.cs
@@ -7,83 +7,55 @@ namespace AutoCalculator
 {
     class Program
     {
-        public static double  seed = 0.0f;
-        public static void SingularityProc()
-        {
-            var charList = new ArrayList();
-
-            var rand = new Random();
-            bool isSeedTriggered = true;
-//            double seed = 0.0f;
-
-            while (true)
-            {
-                if (isSeedTriggered)
-                {
-                    seed = 1;
-                    isSeedTriggered = false;
-                }
-
-                seed = PandoraCalculator.GetTeslaCode(seed);
-                Console.WriteLine(seed);
-            }
-        }
-
-        public static void SingularityProc3()
-        {
-            var charList = new ArrayList();
-
-            var rand = new Random();
-            bool isSeedTriggered = true;
-//            double seed = 0.0f;
-
-            while (true)
-            {
-                if (isSeedTriggered)
-                {
-                    seed = 3;
-                    isSeedTriggered = false;
-                }
-
-                seed = PandoraCalculator.GetTeslaCode(seed);
-                Console.WriteLine(seed);
-            }
-        }
-
-        public static void SingularityProc9()
-        {
-            var charList = new ArrayList();
-
-            var rand = new Random();
-            bool isSeedTriggered = true;
-            double seed = 0.0f;
-
-            while (true)
-            {
-                if (isSeedTriggered)
-                {
-                    seed = 9;
-                    isSeedTriggered = false;
-                }
-
-                seed = PandoraCalculator.GetTeslaCode(seed);
-                Console.WriteLine(seed);
-            }
-        }
-
-
+        public static double seed = 0.0f;
 
         static void Main(string[] args)
         {
-            Thread t1 = new Thread(new ThreadStart(SingularityProc));
-            Thread t2 = new Thread(new ThreadStart(SingularityProc3));
-            Thread t3 = new Thread(new ThreadStart(SingularityProc9));
+            int[,] input = new int[,] { { 1, 0 }, { 1, 1 }, { 0, 1 }, { 0, 0 } };
+            int[] outputs = { 0, 1, 0, 0 };
 
-            t1.Start();
-            t2.Start();
-            t3.Start();
+            Random r = new Random();
 
+            double[] weights = { r.NextDouble(), r.NextDouble(), r.NextDouble() };
+
+            double learningRate = 1;
+            double totalError = 1;
+
+            // 학습
+            while (totalError > 0.2)
+            {
+                totalError = 0;
+                for (int i = 0; i < 4; i++)
+                {
+                    int output = calculateOutput(input[i, 0], input[i, 1], weights);
+
+                    int error = outputs[i] - output;
+
+                    weights[0] += learningRate * error * input[i, 0];
+                    weights[1] += learningRate * error * input[i, 1];
+                    weights[2] += learningRate * error * 1;
+
+                    totalError += Math.Abs(error);
+                }
+
+            }
+
+            Console.WriteLine("Results:");
+
+            // 평가
+            for (int i = 0; i < 4; i++)
+                Console.WriteLine(calculateOutput(input[i, 0], input[i, 1], weights));
+
+            Console.ReadLine();
 
         }
+
+        private static int calculateOutput(double input1, double input2, double[] weights)
+        {
+            double sum = input1 * weights[0] + input2 * weights[1] + 1 * weights[2];
+//            Console.WriteLine(sum);
+            return (sum >= 0) ? 1 : 0;
+        }
+
     }
 }
